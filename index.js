d2882a057d560195152e029834824d23ffbd3230
aashpaash <115119317+aashpaash@users.noreply.github.com>
2023-05-08 19:33:05 -0700

    improved index js file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Node modules
 const inquirer = require('inquirer');
 const fs = require('fs');
-const jest = require('jest');
 
 // Generate team page
 const generateTeam = require('./utils/generateTeam');
@@ -14,109 +13,106 @@ const engineer = require('./lib/engineer');
 
 // Team string
 const team = {
-    employee:[],
-    manager: [],
-    engineer:[],
-    intern: [],
-
-}
-
-// Import jest for resting
-const jest = require('jest');
+  employee: [],
+  manager: [],
+  engineer: [],
+  intern: [],
+};
 
 // Store manager database
 function teamManager() {
-    inquirer.propmt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Name of manager:',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Manager email address:'
-        },
-        {
-            type: 'input',
-            name: 'officeNum',
-            message: 'Manger office number:',
-        },
-        {
-            type: 'input',
-            name:'Id',
-            message: 'Id of manager:',
-        }
-    ]).then(manager => {
-        team.manager = manager;
-    });
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Name of manager:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Manager email address:',
+    },
+    {
+      type: 'input',
+      name: 'officeNum',
+      message: 'Manger office number:',
+    },
+    {
+      type: 'input',
+      name: 'Id',
+      message: 'Id of manager:',
+    },
+  ]).then(manager => {
+    team.manager = manager;
     addMember();
+  });
 }
 
 function addMember() {
-    inquirer.prompt([{
-        type: 'list',
-        message: 'Select role to add:',
-        name: 'roles',
-        choices: [
-            "engineer",
-            "intern",
-            "n/a"
-        ]
+  inquirer.prompt([{
+    type: 'list',
+    message: 'Select role to add:',
+    name: 'roles',
+    choices: [
+      'engineer',
+      'intern',
+      'n/a'
+    ],
+  }]).then(selection => {
+    if (selection.roles === 'engineer') {
+      addEngineer();
+    } if (selection.roles === 'intern') {
+      addIntern();
+    } else {
+      generateTeam(team);
     }
-]).then(selection =>{
-    if (selection.role === 'engineer'){
-        addEngineer();
-    } if (selection.role === 'inrern'){
-        addIntern();
-    } else generateTeam(team)
-})
+  });
 }
 
-function addEnginier(){
-    inquirer.propmpt([{
-        type: 'input',
-        name: 'name',
-        message: 'Name of engineer:',
+function addEngineer() {
+  inquirer.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Name of engineer:',
     },
     {
-        type: 'input',
-        name: 'email',
-        message: 'engineer email address:',
+      type: 'input',
+      name: 'email',
+      message: 'engineer email address:',
     },
     {
-        type: 'input',
-        name: 'ID',
-        message: 'engineer ID:',
+      type: 'input',
+      name: 'ID',
+      message: 'engineer ID:',
     },
-    
-    ]).then(enginieer => {
-        team.engineer.push(engineer);
-        addMember();
-    })
+
+  ]).then(engineer => {
+    team.engineer.push(engineer);
+    addMember();
+  });
 }
 
-function addIntern(){
-    inquirer.propmpt([{
-        type: 'input',
-        name: 'name',
-        message: 'Name of intern:',
+function addIntern() {
+  inquirer.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Name of intern:',
     },
     {
-        type: 'input',
-        name: 'school',
-        message: 'Name of school:',
+      type: 'input',
+      name: 'school',
+      message: 'Name of school:',
     },
     {
-        type: 'input',
-        name: 'ID',
-        message: 'engineer ID:',
+      type: 'input',
+      name: 'ID',
+      message: 'intern ID:',
     },
-    
-    ]).then(intern => {
-        team.intern.push(engineer);
-        addMember();
-    })
+
+  ]).then(intern => {
+    team.intern.push(intern);
+    addMember();
+  });
 }
 
-addMember();
+teamManager();
